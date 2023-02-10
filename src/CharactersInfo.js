3156d3b6fc2a478343fb2cf9f96571985818b4e6
carojp <carojp62@gmail.com>
2023-02-10 16:38:36 -0500

    commit en index.html se indican los estilos de fuente

--- src/CharactersInfo.js
+++ src/CharactersInfo.js
@@ -2,7 +2,11 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function CharactersInfo() {
-  return console.log("Hola Rick & Morty");
+  return (
+    <section>
+      <h2>Name</h2>
+    </section>
+  );
 }
 
 export default CharactersInfo;
