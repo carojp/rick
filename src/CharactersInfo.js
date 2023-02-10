3fc8b7fec09086eb427dda86cfc4d5af80803060
carojp <carojp62@gmail.com>
2023-02-10 17:44:34 -0500

    commit componente CharacterInfo

--- src/CharactersInfo.js
+++ src/CharactersInfo.js
@@ -1,10 +1,17 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./styles/CharactersInfo.css";
+import Square from "./assets/Square.png";
 
 function CharactersInfo() {
   return (
-    <section>
-      <h2>Name</h2>
+    <section className="card-info">
+      <h2 className="character-name">Name</h2>
+      <img className="character-image" src={Square} alt="" />
+      <div className="character-info">
+        <h2 className="character-gender">Gender</h2>
+        <h2 className="character-status">Status</h2>
+      </div>
     </section>
   );
 }
